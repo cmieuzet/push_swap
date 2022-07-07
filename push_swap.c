99a4c88a8924839a2f13da0aa4279c98cd9cae06
Corentin Mieuzet <cmieuzet@3G2.42angouleme.fr>
2022-07-07 11:36:49 +0200

    explication

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cmieuzet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 19:17:03 by cmieuzet          #+#    #+#             */
-/*   Updated: 2022/07/05 17:33:14 by cmieuzet         ###   ########.fr       */
+/*   Updated: 2022/07/07 11:36:30 by cmieuzet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,20 @@
 
 int	main(int argc, char **argv)
 {
-	t_pile	*pile_a;
+	t_pile	pile_a;
 
-	if (!ft_create_pile(argc, argv))
+	pile_a = NULL;
+
+	if (!ft_create_pile(argc, argv, &pile_a))
 	{
 		ft_putstr_fd("Error\n", 1);
 		return (0);
 	}
+	ft_program -- fait le tri
+
 }
+
+t_element	*element;
+
+
+
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cmieuzet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 19:14:00 by cmieuzet          #+#    #+#             */
-/*   Updated: 2022/07/05 17:17:41 by cmieuzet         ###   ########.fr       */
+/*   Updated: 2022/07/07 11:36:28 by cmieuzet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,25 @@ typedef struct s_pile
 }			t_pile;
 
 #endif
+*pile_a->
+*t_element(mallloc) t_element *element; 
+pile_a->top = element;
+
+top(malloc)  / premier elemt mom mallloc top et
+top->next = element;
+top = top->next;
+top->next = element;
+element = ft_atoi(number) a passser ds la fonction qui cree un element
+&element -- a add back
+top = top->next;
+
+
+in create pile t_element *element a malloc
+
+t_element *tmp;
+
+tmp = pile_a.top;
+
+pile_a la pile non malloc
+pile_a.top premier element malloc 
+
