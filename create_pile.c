120d52b7f5ab2eecc206bb59688e11aaf82ccc59
Corentin Mieuzet <cmieuzet@3G2.42angouleme.fr>
2022-07-07 12:23:44 +0200

    explication

--- create_pile.c
+++ create_pile.c
@@ -1,33 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.c                                            :+:      :+:    :+:   */
+/*   create_pile.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cmieuzet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 19:52:44 by cmieuzet          #+#    #+#             */
-/*   Updated: 2022/06/28 19:52:47 by cmieuzet         ###   ########.fr       */
+/*   Updated: 2022/07/07 12:23:18 by cmieuzet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_pile	*ft_create_pile(int argc, char **argv)
+int	ft_create_pile(int argc, char **argv, t_pile *pile_a)
 {
-	t_pile	pile_a;
 	int	i;
 
 	i = 1;
 	if (argc < 2)
 		return (NULL);
 	if (argc == 2)
-		pile_a = ft_error_one(argv);
+		ft_error_one(argv, pile_a);
 	else
 	{
 		while (i <= argc)
 		{
-			pile_a = ft_error_many(argv[i], &pile_a);
+			ft_error_many(argv[i], &pile_a);
 			i++;
 		}
 	}
 }
+
+return int error = same pour les ft_error;
